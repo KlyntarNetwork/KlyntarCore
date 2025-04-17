08236b368b94b19f72adafb424260d871604e727
VladChernenko <vladchernenko@protonmail.com>
2025-04-17 08:43:29 +0300

    Config option removed

--- KLY_Workflows/dev_tachyon/common_functions/logging.js
+++ KLY_Workflows/dev_tachyon/common_functions/logging.js
@@ -1,5 +1,3 @@
-import {CONFIGURATION} from '../../../klyntar_core.js'
-
 import {logColors} from '../../../KLY_Utils/utils.js'
 
 import {WORKING_THREADS} from '../globals.js'
@@ -42,25 +40,20 @@ export let vtStatsLog = (epochFullID,currentLeader,blockIndex,blockHash,txsNumbe
 
 export let blockLog = (msg,hash,block,epochIndex) => {
 
+    let preColor = msg.includes('accepted') ? '\x1b[31m' : '\x1b[32m'
 
-    if(CONFIGURATION.NODE_LEVEL.DAEMON_LOGS){
+    console.log(logColors.TIME_COLOR,`[${new Date().toLocaleDateString()} at ${new Date().toLocaleTimeString()}]\u001b[38;5;99m(pid:${process.pid})`,logColors.CYAN,msg,logColors.CLEAR)
 
-        let preColor = msg.includes('accepted') ? '\x1b[31m' : '\x1b[32m'
-
-        console.log(logColors.TIME_COLOR,`[${new Date().toLocaleDateString()} at ${new Date().toLocaleTimeString()}]\u001b[38;5;99m(pid:${process.pid})`,logColors.CYAN,msg,logColors.CLEAR)
-
-        console.log('\n')
-        
-        console.log(` ${preColor}│\x1b[33m  ID:\x1b[36;1m`,epochIndex+':'+block.creator+':'+block.index,logColors.CLEAR)
-
-        console.log(` ${preColor}│\x1b[33m  Hash:\x1b[36;1m`,hash,logColors.CLEAR)
-
-        console.log(` ${preColor}│\x1b[33m  Txs:\x1b[36;1m`,block.transactions.length,logColors.CLEAR)
-
-        console.log(` ${preColor}│\x1b[33m  Time:\x1b[36;1m`,new Date(block.time).toString(),logColors.CLEAR)
+    console.log('\n')
     
-        console.log('\n')
+    console.log(` ${preColor}│\x1b[33m  ID:\x1b[36;1m`,epochIndex+':'+block.creator+':'+block.index,logColors.CLEAR)
 
-    }
+    console.log(` ${preColor}│\x1b[33m  Hash:\x1b[36;1m`,hash,logColors.CLEAR)
+
+    console.log(` ${preColor}│\x1b[33m  Txs:\x1b[36;1m`,block.transactions.length,logColors.CLEAR)
+
+    console.log(` ${preColor}│\x1b[33m  Time:\x1b[36;1m`,new Date(block.time).toString(),logColors.CLEAR)
+
+    console.log('\n')
 
 }
