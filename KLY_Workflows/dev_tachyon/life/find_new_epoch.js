d2e5f269a25660af20fc834e429b382cf3b4957d
VladChernenko <vladchernenko@protonmail.com>
2025-04-23 15:14:33 +0300

    Fix

--- KLY_Workflows/dev_tachyon/life/find_new_epoch.js
+++ KLY_Workflows/dev_tachyon/life/find_new_epoch.js
@@ -261,9 +261,7 @@ export let startEpochRotationThread=async()=>{
                 }
 
 
-                let firstBlocksHashes = []
-
-                firstBlocksHashes.push(aefpAndFirstBlockData.firstBlockHash)
+                let firstBlockHash = aefpAndFirstBlockData.firstBlockHash
 
                 
                 // For API - store the whole epoch handler object by epoch numerical index
@@ -319,7 +317,7 @@ export let startEpochRotationThread=async()=>{
                 
                 let nextEpochId = currentEpochHandler.id + 1
 
-                let nextEpochHash = blake3Hash(JSON.stringify(firstBlocksHashes))
+                let nextEpochHash = blake3Hash(firstBlockHash)
 
 
                 copyOfApprovementThread.EPOCH.id = nextEpochId
