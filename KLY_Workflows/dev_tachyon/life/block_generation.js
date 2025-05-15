74b4e2b9730e1dfb478adfba7c3fb907a1e7a038
VladChernenko <vladchernenko@protonmail.com>
2025-05-15 07:57:44 +0300

    Bugfix in ALRP find process

--- KLY_Workflows/dev_tachyon/life/block_generation.js
+++ KLY_Workflows/dev_tachyon/life/block_generation.js
@@ -540,7 +540,7 @@ let generateBlocksPortion = async() => {
 
                 let votingFinalizationPerPool = await BLOCKCHAIN_DATABASES.FINALIZATION_VOTING_STATS.get(epochIndex+':'+leaderPubKey).catch(()=>({index:-1,hash:'0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef',afp:{}}))
 
-                let proofThatAtLeastFirstBlockWasCreated = votingFinalizationPerPool.index !== 0
+                let proofThatAtLeastFirstBlockWasCreated = votingFinalizationPerPool.index >= 0
 
                 // We 100% need ALRP for previous pool
                 // But no need in pools who created at least one block in epoch and it's not our previous pool
